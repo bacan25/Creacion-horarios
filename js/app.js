c0dbd361e8bdde8a93723269dc89ca786752d808
bacan25 <126129588+bacan25@users.noreply.github.com>
2023-03-24 15:42:41 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@ const jueves_input = document.getElementById("jueves");
 const viernes_input = document.getElementById("viernes");
 const sabado_input = document.getElementById("sabado");
 const domingo_input = document.getElementById("domingo");
-const horario_input = document.getElementById("horario");
+const horario_input1 = document.getElementById("horarioInicio");
+const horario_input2 = document.getElementById("horarioFinal");
 
 async function add_form(e){
     e.preventDefault();
@@ -20,7 +21,9 @@ async function add_form(e){
     const viernes = viernes_input.checked;
     const sabado = sabado_input.checked;
     const domingo = domingo_input.checked;
-    const horario = horario_input.value;
+    const horario1 = horario_input1.value;
+    const horario2 = horario_input2.value;
+
     const data = {
         nombre: nombre,
         lunes: lunes,
@@ -30,13 +33,22 @@ async function add_form(e){
         viernes: viernes,
         sabado: sabado,
         domingo: domingo,
-        horario: horario
+        horario1: horario1,
+        horario2: horario2,
+
     };
     await insert("integrantes",data);
     // console.log(data);
     const user_form = document.getElementById("formulario");
     user_form.reset();
 }
-
+async function read_form(){
+    const response = await read("integrantes");
+    response.forEach(element => {
+        console.log(element.data());
+    })
+}
+const btn = document.getElementById("log");
+btn.addEventListener("click", read_form);
 user_form.addEventListener("submit", add_form);
 
